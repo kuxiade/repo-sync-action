b30c88d475be7d1be52ea46644d06917a9444423
kuxia <kuxiade@hotmail.com>
2020-10-31 00:22:02 +0800

    优化

--- entrypoint.sh
+++ entrypoint.sh
@@ -239,5 +239,5 @@ fi
 git remote set-url --push origin "$DESTINATION_REPO"
 git fetch -p origin
 # Exclude refs created by GitHub for pull request.
-git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
+#git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
 git push --mirror
