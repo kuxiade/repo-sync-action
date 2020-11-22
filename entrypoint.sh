5f540c4605540905cfdeaa766f5869379b35d8d2
kuxia <kuxiade@hotmail.com>
2020-11-22 20:12:43 +0800

    优化

--- entrypoint.sh
+++ entrypoint.sh
@@ -519,13 +519,14 @@ entrypoint_main() {
         fi
 
         git remote set-url --push origin "$DST_REPO_URL"
-        git fetch -p origin
+        #git fetch -p origin
+        git pull -p
         # 需要删除默认分支，不然推送到目的端时，会创建一个HEAD分支。待确定该如何设置
         #git remote set-head origin --delete
         echo_color cyan "------------------> git push..."
         #git push origin "refs/remotes/origin/*:refs/heads/*" --tags --force --prune
-        git push origin --all
-        git push origin --tags
+        git push origin --all --prune
+        git push origin --tags --prune
     fi
 
 }
