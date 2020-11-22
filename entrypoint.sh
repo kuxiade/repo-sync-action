af47860e139428b180a558a715d4e42f12ee5da8
kuxia <kuxiade@hotmail.com>
2020-11-23 00:22:52 +0800

    优化

--- entrypoint.sh
+++ entrypoint.sh
@@ -529,9 +529,9 @@ entrypoint_main() {
         #git push origin "refs/remotes/origin/*:refs/heads/*" --tags --force --prune
         git --version
         # 推送所有分支
-        git push --all origin
+        git push --all origin --force
         # 推送所有标签
-        git push origin --tags
+        git push origin --tags --force
     fi
 
 }
